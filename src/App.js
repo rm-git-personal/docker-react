4fec21a60e0fdc78b2dc98950b80e92f7e0bd0cf
Martin,Richard <mechanikhil@gmail.com>
2020-11-17 22:43:31 -0600

    changed source 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi There!.
+          Simba!.
         </p>
         <a
           className="App-link"
